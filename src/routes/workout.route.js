ebfd0073cb4b1325c1b68194681d38551ecf8632
hanhukoe <hanhukoe@gmail.com>
2025-07-13 19:00:52 +0200

    Update workout.route.js
    add new router

--- src/routes/workout.route.js
+++ src/routes/workout.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { generateWorkoutPlan } from "../controllers/workout.controller.js";
+import { generateWorkoutPlan, processProgramFromLog } from "../controllers/workout.controller.js";
 
 const router = express.Router();
 
 router.post("/generate", generateWorkoutPlan);
+router.post("/process-log", processProgramFromLog); // 🆕
 
 export default router;
