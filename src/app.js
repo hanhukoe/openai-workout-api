5557934df95b1799cce7ff5154606b23dd4ec05c
hanhukoe <hanhukoe@gmail.com>
2025-07-13 11:10:14 +0200

    Update app.js
    add comments

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import workoutRoutes from "./routes/workout.route.js";
 
 const app = express();
 
+// Middleware to parse incoming JSON bodies
 app.use(express.json());
+
+// Mounts routes defined in workout.route.js at "/workout"
 app.use("/workout", workoutRoutes);
 
 export default app;
