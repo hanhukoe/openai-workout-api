871a4848f333ab8216893ef9c7486e959203213c
hanhukoe <hanhukoe@gmail.com>
2025-07-13 09:51:18 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+import express from "express";
+import workoutRoutes from "./routes/workout.route.js";
 
+const app = express();
+
+app.use(express.json());
+app.use("/workout", workoutRoutes);
+
+export default app;
